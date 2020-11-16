7af3e3baaf02853d6b1bb42a4a4b8f83fa845b1c
Egor Yudin <eydnep2000@gmail.com>
2020-11-16 17:23:21 +0200

    Assignment model and migrations reworked.

---
+++ src/dataBase/models/assignment.js
@@ -0,0 +1,79 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Assignment extends Model {
+    static associate(models) {
+      Assignment.belongsTo(models.Location, {foreignKey: 'locationId'});
+      Assignment.belongsTo(models.User, {foreignKey: 'authorTelegramId', as: 'author'});
+
+      Assignment.belongsToMany(models.User, {through: 'FavoriteAssignments', foreignKey: 'assignmentId', as: 'favoriteUsers'});
+    }
+  };
+  Assignment.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    authorTelegramId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'telegramId'
+      }
+    },
+    locationId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Locations',
+        key: 'id'
+      }
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    reward: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    link: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: ['Не выполнено', 'Выполнено'],
+      defaultValue: 'Не выполнено'
+    },
+    pictureUrl: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    spamScore: {
+      allowNull: false,
+      defaultValue: 0,
+      type: DataTypes.INTEGER
+    },
+    category: {
+      allowNull: false,
+      type: DataTypes.ENUM,
+      values: ['Помощь', 'Обмен', 'Ремонт', 'Обучение', 'Другое'],
+      defaultValue: 'Другое'
+    }
+  }, {
+    sequelize,
+    modelName: 'Assignment',
+  });
+  return Assignment;
+};

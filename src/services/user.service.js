0831329b169a7d8a2d927283f40b2591c5e9bcba
Egor Yudin <eydnep2000@gmail.com>
2020-11-20 18:08:12 +0200

    User service example was commented.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,26 +1,26 @@
-const {
-    models: {
-        user
-    }
-} = require('../dataBase');
-
-module.exports = {
-    create(user) {
-        return user.create(user);
-    },
-    update(id, updateFields) {
-        return user.update(updateFields, {
-            where: {id}
-        });
-    },
-    delete(id) {
-        return user.destroy({
-            where: {id}
-        });
-    },
-    getById(id) {
-        return user.findOne({
-            where: {id}
-        });
-    }
-}
+// const {
+//     models: {
+//         user
+//     }
+// } = require('../dataBase');
+//
+// module.exports = {
+//     create(user) {
+//         return user.create(user);
+//     },
+//     update(id, updateFields) {
+//         return user.update(updateFields, {
+//             where: {id}
+//         });
+//     },
+//     delete(id) {
+//         return user.destroy({
+//             where: {id}
+//         });
+//     },
+//     getById(id) {
+//         return user.findOne({
+//             where: {id}
+//         });
+//     }
+// }

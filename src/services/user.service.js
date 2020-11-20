cdbad9cf63926a3f1e7536813f72a8f0cdaf4e9c
Egor Yudin <eydnep2000@gmail.com>
2020-11-20 03:27:02 +0200

    User range update method was implemented in services.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -27,4 +27,27 @@ module.exports = {
             return new ServiceResponse ( { succeeded: false, message: `Error occurred while creating user with telegramId=${telegramId}.` } )
         }
     },
+
+
+    async updateRange(telegramId, newRange) {
+        try {
+            const foundUser = await User.findOne({
+                where: {
+                    telegramId: telegramId
+                }
+            })
+
+            if (foundUser) {
+                const result = await foundUser.update({
+                    range: newRange
+                })
+
+                if (result._previousDataValues.range !== result.dataValues.range) return new ServiceResponse( { succeeded: true} )
+                return new ServiceResponse( { succeeded: true, message: `Range of user with telegramId=${telegramId} was not updated.`} )
+            }
+            return new ServiceResponse( { succeeded: true, message: `User with such telegramId=${telegramId} was not found.` } )
+        } catch (e) {
+            return new ServiceResponse ( { succeeded: false, message: `Error occurred while updating user with telegramId=${telegramId}` } )
+        }
+    }
 }

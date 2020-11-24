a3790b537c6b94f8fd1da89d79f23bb254d26eb9
GorgeousPuree <eydnep2000@gmail.com>
2020-11-24 13:01:18 +0200

    eslint last migration.

--- src/database/migrations/20201124103803-change-users-default-language.js
+++ src/database/migrations/20201124103803-change-users-default-language.js
@@ -1,5 +1,3 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.sequelize.query('ALTER TABLE ONLY "Users" ALTER COLUMN locale SET DEFAULT \'en\';');

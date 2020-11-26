0b222905e344d0f71fc4fa9649dd100aa9905fea
maksim-luzin <maksim-luzin@users.noreply.github.com>
2020-11-27 00:39:10 +0200

    Add start constant.

---
+++ src/constants/button.text.js
@@ -0,0 +1,7 @@
+exports.BUTTON_DEFAULT_MENU_START = 'Старт';
+exports.BUTTON_MAIN_MENU_ABOUT_US = 'О нас';
+exports.BUTTON_MAIN_MENU_ADD_LOCATION = 'Добавить локацию';
+exports.BUTTON_ADD_LOCATION_MENU_ADD_LOCATION = 'Отправить локацию';
+exports.BUTTON_ADD_LOCATION_MENU_ADD_LOCATION_HOME = 'Дом';
+exports.BUTTON_ADD_LOCATION_MENU_ADD_LOCATION_WORK = 'Работа';
+exports.BUTTON_ALL_MENU_CANCEL = 'Отмена';

edec93f1eaf2dd753b787ed8d4cbf226dff12803
Egor Yudin <eydnep2000@gmail.com>
2020-11-26 20:17:37 +0200

    'dotenv' was moved, Dasha credentials were removed.

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 module.exports.global = require('./global.config');
 module.exports.sequelize = require('./sequelize.config');
 module.exports.winstonOptions = require('./winston.config');

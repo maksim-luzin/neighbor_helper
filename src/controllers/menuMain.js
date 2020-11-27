8f141fe5877fc9132a6c53fb7dccaafbf38a0cc6
maksim-luzin <64692611+maksim-luzin@users.noreply.github.com>
2020-11-27 08:05:49 +0200

    Edit menuMain.

--- src/controllers/menuMain.js
+++ src/controllers/menuMain.js
@@ -1,12 +1,12 @@
 const { mainMenu, mainMenuMessage } = require('../templates/mainMenu');
 const { templateResponse } = require('../middlewares');
 const { COMMAND_START } = require('../constants/comand');
-const { UserService } = require('../services');
+const { userService } = require('../services');
 
 const menuMain = async (message) => {
   if (message.text !== COMMAND_START) return templateResponse(mainMenu, mainMenuMessage(message));
 
-  await UserService.addUser(message.from.id);
+  await userService.create({telegramId: message.from.id});
   return templateResponse(mainMenu, mainMenuMessage(message));
 };
 

c3dbbca1676322fc001e6568c7106ca8097a018b
maksim-luzin <maksim-luzin@users.noreply.github.com>
2020-11-28 17:47:24 +0200

    Edit constants.

--- src/constants/button.text.js
+++ src/constants/button.text.js
@@ -1,5 +1,4 @@
-exports.BUTTON_DEFAULT_MENU_START = 'Старт';
-exports.BUTTON_MAIN_MENU_ABOUT_US = 'О нас';
-exports.BUTTON_MAIN_MENU_ADD_LOCATION = 'Добавить локацию';
-exports.BUTTON_ADD_LOCATION_MENU_ADD_LOCATION = 'Отправить локацию';
-exports.BUTTON_ALL_MENU_CANCEL = 'Отмена';
+exports.MAIN_MENU = {
+  BUTTON_ABOUT_US: 'О нас',
+  BUTTON_ADD_LOCATION: 'Добавить локацию',
+};

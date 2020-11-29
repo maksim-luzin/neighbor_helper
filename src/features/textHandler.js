92b4e9aff0f0c3ab8a2f6a4058d57584bac1dfc0
Egor Yudin <eydnep2000@gmail.com>
2020-11-30 00:59:19 +0200

    New case was added in textHandler.js (range).

--- src/features/textHandler.js
+++ src/features/textHandler.js
@@ -1,23 +1,30 @@
 const {
   BUTTON_ABOUT_US,
   BUTTON_ADD_LOCATION,
+  BUTTON_CHANGE_RANGE,
 } = require('../constants/button.text').MAIN_MENU;
 
 const {
   startAction,
   mainMenuAction,
   aboutUsAction,
+  showRangeAction,
 } = require('../actions/mainActions');
 
 const textHandlers = async (request) => {
+  let response;
   switch (request.text) {
     case '/start':
       // eslint-disable-next-line no-case-declarations
-      const response = await startAction(request);
+      response = await startAction(request);
       return response;
 
     case BUTTON_ABOUT_US:
       return aboutUsAction();
+
+    case BUTTON_CHANGE_RANGE:
+      response = await showRangeAction(request);
+      return response;
 
     default:
       return mainMenuAction();

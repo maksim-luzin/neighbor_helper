a93cf9596b5d10e21b83922db69e826b71817af1
paratskiy <bog4242@gmail.com>
2020-12-01 23:11:03 +0200

    create assignment-template

---
+++ src/templates/assignmentTemplate/assignmentInlineKeyboardTemplate.js
@@ -0,0 +1,30 @@
+const ownAssignmentInlineKeyboardTemplate = (status) => {
+  const visibilityEvent = (status === 'done')
+    ? { text: 'Отметить как невыполненное', callback_data: 'markAssignmentAsNotCompleted' }
+    : { text: 'Отметить как выполненное', callback_data: 'markAssignmentAsCompletedAction' };
+
+  return [
+    [visibilityEvent],
+    [{ text: 'Редактировать', callback_data: 'editAssignmentAction' }],
+    [{ text: 'Удалить', callback_data: 'removeAssignmentAction' }],
+  ];
+};
+
+const publicAssignmentInlineKeyboardTemplate = ({ isFavorite }) => {
+  const favoriteEvent = isFavorite
+    ? { text: 'Убрать из избранных', callback_data: 'removeFromFavoritesAction' }
+    : { text: 'В избранные', callback_data: 'addToFavoritesAction' };
+
+  return [
+    [{ text: 'Связаться с автором', callback_data: 'contactTheAuthorAction' }],
+    [
+      favoriteEvent,
+      { text: 'Spam', callback_data: 'markAsSpamAction' },
+    ],
+  ];
+};
+
+module.exports = {
+  ownAssignmentInlineKeyboardTemplate,
+  publicAssignmentInlineKeyboardTemplate,
+};

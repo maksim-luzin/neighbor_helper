b4c1100540aee89f7fa6b369d9b315d21871589b
paratskiy <bog4242@gmail.com>
2020-12-01 16:31:41 +0200

    create assignment templates

---
+++ src/templates/assignmentTemplate/index.js
@@ -0,0 +1,4 @@
+exports.myAssignmentsKeyboardTemplate = require('./myAssignmentsKeyboardTemplate');
+exports.asignmentTextTemplate = require('./assignmentTextTemplate');
+exports.ownAssignmentInlineKeyboardTemplate = require('./assignmentInlineKeyboardTemplate').ownAssignmentInlineKeyboardTemplate;
+exports.publicAssignmentInlineKeyboardTemplate = require('./assignmentInlineKeyboardTemplate').publicAssignmentInlineKeyboardTemplate;

68bea3474a5e13e5ef9f2aed603e8d8e428b4cb4
Egor Yudin <eydnep2000@gmail.com>
2020-12-02 16:31:58 +0200

    new buttons constants were added.

--- src/constants/button.text.js
+++ src/constants/button.text.js
@@ -2,6 +2,7 @@ exports.MAIN_MENU = {
   BUTTON_ABOUT_US: 'О нас',
   BUTTON_ADD_LOCATION: 'Добавить локацию',
   BUTTON_CHANGE_RANGE: 'Редактировать радиус',
+  BUTTON_FIND_ASSIGNMENT: 'Найти объявление',
 };
 
 exports.CHANGE_RANGE = {
@@ -12,4 +13,12 @@ exports.CHANGE_RANGE = {
 exports.COMMON = {
   BUTTON_BACK: 'Назад',
   BUTTON_HOME: 'Домой',
+  BUTTON_SKIP: 'Пропустить',
+};
+
+exports.CATEGORY = {
+  BUTTON_BARTER: 'Обмен',
+  BUTTON_EDUCATION: 'Обучение',
+  BUTTON_HELP: 'Помощь',
+  BUTTON_REPAIR: 'Ремонт',
 };

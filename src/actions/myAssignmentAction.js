9fba490fa60fc68c9eb3406e321f3fb52af98201
paratskiy <bog4242@gmail.com>
2020-12-02 17:51:10 +0200

    add myAssignments actions

--- src/actions/myAssignmentAction.js
+++ src/actions/myAssignmentAction.js
@@ -1,43 +1,52 @@
 const { telegramTemplate } = require('claudia-bot-builder');
+const { assignmentService } = require('../services');
 const {
   asignmentTextTemplate,
   ownAssignmentInlineKeyboardTemplate,
   publicAssignmentInlineKeyboardTemplate,
 } = require('../templates/assignmentTemplate');
 
-const favoriteAssignmentsAction = () => (
-  new telegramTemplate
-    .Photo(
-      'https://claudiajs.com/assets/claudiajs.png',
-      asignmentTextTemplate({
-        title: 'Требуеться помощь по ремонту',
-        description: 'Прошу помочь с установкой межкомнатной двери',
-        reward: null,
-        authorTelegramId: '123',
-        pictureUrl: 'https://claudiajs.com/assets/claudiajs.png',
-        locationName: 'Дом',
-      }),
-    ).addInlineKeyboard(publicAssignmentInlineKeyboardTemplate({ isFavorite: false }))
-    .get()
-);
+const favoriteAssignmentsAction = async (request) => {
+  const result = await assignmentService.getAllFavorites({
+    telegramId: request.from.id,
+  });
 
-const ownAssignmentsAction = () => (
-  new telegramTemplate
-    .Photo(
-      'https://claudiajs.com/assets/claudiajs.png',
-      asignmentTextTemplate({
-        title: 'Требуеться помощь по ремонту',
-        description: 'Прошу помочь с установкой межкомнатной двери',
-        reward: null,
-        authorTelegramId: '123',
-        pictureUrl: 'https://claudiajs.com/assets/claudiajs.png',
-        locationName: 'Дом',
-      }),
-    ).addInlineKeyboard(ownAssignmentInlineKeyboardTemplate('done'))
-    .get()
-);
+  const assignments = result.model;
+
+  return assignments.map((assignment) => {
+    if (assignment.pictureUrl) {
+      return new telegramTemplate.Photo(assignment.pictureUrl, asignmentTextTemplate(assignment))
+        .addInlineKeyboard(publicAssignmentInlineKeyboardTemplate({ isFavorite: true }))
+        .get();
+    }
+
+    return new telegramTemplate.Text(asignmentTextTemplate(assignment))
+      .addInlineKeyboard(publicAssignmentInlineKeyboardTemplate({ isFavorite: true }))
+      .get();
+  });
+};
+
+const createdAssignmentsAction = async (request) => {
+  const result = await assignmentService.getCreated({
+    telegramId: request.from.id,
+  });
+
+  const assignments = result.model;
+
+  return assignments.map((assignment) => {
+    if (assignment.pictureUrl) {
+      return new telegramTemplate.Photo(assignment.pictureUrl, asignmentTextTemplate(assignment))
+        .addInlineKeyboard(ownAssignmentInlineKeyboardTemplate(assignment.status))
+        .get();
+    }
+
+    return new telegramTemplate.Text(asignmentTextTemplate(assignment))
+      .addInlineKeyboard(ownAssignmentInlineKeyboardTemplate(assignment.status))
+      .get();
+  });
+};
 
 module.exports = {
   favoriteAssignmentsAction,
-  ownAssignmentsAction,
+  createdAssignmentsAction,
 };

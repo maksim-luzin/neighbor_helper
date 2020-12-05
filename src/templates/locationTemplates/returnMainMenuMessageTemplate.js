9e0033baea17666ea0013221b98dbd2a8e53030e
maksim-luzin <maksim-luzin@users.noreply.github.com>
2020-12-06 01:42:04 +0200

    Create, update location.

---
+++ src/templates/locationTemplates/returnMainMenuMessageTemplate.js
@@ -0,0 +1,7 @@
+const returnMainMenuMessageAfterCreateLocationTemplate = 'Локация\nуспешно создана.';
+const returnMainMenuMessageAfterUpdateLocationTemplate = 'Локация\nуспешно обновлена.';
+
+module.exports = {
+  returnMainMenuMessageAfterCreateLocationTemplate,
+  returnMainMenuMessageAfterUpdateLocationTemplate,
+};

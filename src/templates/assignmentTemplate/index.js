5dcc39cce9e8d7ad774651279047e3a585702a93
Egor Yudin <eydnep2000@gmail.com>
2020-12-08 04:14:21 +0200

    New assignment templates were added.

--- src/templates/assignmentTemplate/index.js
+++ src/templates/assignmentTemplate/index.js
@@ -1,4 +1,6 @@
 exports.myAssignmentsKeyboardTemplate = require('./myAssignmentsKeyboardTemplate');
-exports.asignmentTextTemplate = require('./assignmentTextTemplate');
+exports.assignmentTextTemplate = require('./assignmentMessageTemplate');
 exports.ownAssignmentInlineKeyboardTemplate = require('./assignmentInlineKeyboardTemplate').ownAssignmentInlineKeyboardTemplate;
 exports.publicAssignmentInlineKeyboardTemplate = require('./assignmentInlineKeyboardTemplate').publicAssignmentInlineKeyboardTemplate;
+exports.findAssignmentsMessageTemplate = require('./findAssignmentsMessageTemplate');
+exports.findAssignmentsKeyboardTemplate = require('./findAssignmentsKeyboardTemplate');

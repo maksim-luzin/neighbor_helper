b6d8cb5355af9e7d1b7df628e97c23d746e1085d
paratskiy <bog4242@gmail.com>
2020-12-12 23:36:55 +0200

    add assignmentId to keyboard template for deleting

--- src/templates/assignmentTemplate/assignmentInlineKeyboardTemplate.js
+++ src/templates/assignmentTemplate/assignmentInlineKeyboardTemplate.js
@@ -1,4 +1,4 @@
-const ownAssignmentInlineKeyboardTemplate = (status) => {
+const ownAssignmentInlineKeyboardTemplate = ({ assignmentId, status }) => {
   const visibilityEvent = (status === 'done')
     ? { text: 'Отметить как невыполненное', callback_data: 'markAssignmentAsNotCompletedAction' }
     : { text: 'Отметить как выполненное', callback_data: 'markAssignmentAsCompletedAction' };
@@ -6,7 +6,7 @@ const ownAssignmentInlineKeyboardTemplate = (status) => {
   return [
     [visibilityEvent],
     [{ text: 'Редактировать', callback_data: 'editAssignmentAction' }],
-    [{ text: 'Удалить', callback_data: 'removeAssignmentAction' }],
+    [{ text: 'Удалить', callback_data: `removeAssignmentAction.${assignmentId}` }],
   ];
 };
 

bc604bf1dce0663b8c7c31228f56f7b77414d759
Egor Yudin <eydnep2000@gmail.com>
2020-12-12 02:37:53 +0200

    New template for range was added.

---
+++ src/templates/rangeTemplate/rangeResponseMessageTemplate.js
@@ -0,0 +1,6 @@
+const rangeResponseMessageTemplate = {
+  rangeErrorResponseMessageTemplate: 'Недопустимое значение радиуса.',
+  rangeSuccessResponseMessageTemplate: 'Радиус успешно изменён.',
+};
+
+module.exports = rangeResponseMessageTemplate;

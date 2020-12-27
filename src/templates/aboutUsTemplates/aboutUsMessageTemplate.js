4a3b57c11494763528ee1d7b5b823110a3970e2b
paratskiy <bog4242@gmail.com>
2020-12-28 01:34:07 +0200

    add localization to about us templates

--- src/templates/aboutUsTemplates/aboutUsMessageTemplate.js
+++ src/templates/aboutUsTemplates/aboutUsMessageTemplate.js
@@ -1,3 +1,5 @@
-const aboutUsMessageTemplate = 'Я бот для организации взаимопомощи соседей.';
+const i18n = require('../../helpers/localization');
+
+const aboutUsMessageTemplate = i18n.t('messages.about');
 
 module.exports = aboutUsMessageTemplate;

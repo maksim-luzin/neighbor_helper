8d25bbeda7a21b47968e54ccf267e003b98fd9bb
paratskiy <bog4242@gmail.com>
2020-12-28 12:57:38 +0200

    add localization to pagination

--- src/templates/paginationTemplates/paginationKeyboardTemplate.js
+++ src/templates/paginationTemplates/paginationKeyboardTemplate.js
@@ -1,11 +1,12 @@
 const { COMMON_ACTIONS } = require('../../constants/actions');
+const i18n = require('../../helpers/localization');
 
 const paginationKeyboardTemplate = ({ currentPage, totalPages }) => {
   const nextPage = (currentPage === totalPages - 1) ? 0 : currentPage + 1;
 
   return [
     [
-      { text: 'Показать еще', callback_data: `${COMMON_ACTIONS.PAGINATION}.${nextPage}` },
+      { text: i18n.t('pagination.showMore'), callback_data: `${COMMON_ACTIONS.PAGINATION}.${nextPage}` },
     ],
   ];
 };

ec9c101f1fc265c5a3dc32b10205d5a218e9f49b
paratskiy <bog4242@gmail.com>
2020-12-28 13:24:03 +0200

    add localization to main menu templates

--- src/templates/mainMenuTemplates/greetingsMessageTemplate.js
+++ src/templates/mainMenuTemplates/greetingsMessageTemplate.js
@@ -1,3 +1,5 @@
-const greetingsMessageTemplate = (firstName) => `Здравствуй, ${firstName}.`;
+const i18n = require('../../helpers/localization');
+
+const greetingsMessageTemplate = (firstName) => i18n.t('messages.greeting', { firstName });
 
 module.exports = greetingsMessageTemplate;
--- src/templates/mainMenuTemplates/mainMenuMessageTemplate.js
+++ src/templates/mainMenuTemplates/mainMenuMessageTemplate.js
@@ -1,3 +1,5 @@
-const mainMenuMessageTemplate = 'Главное меню';
+const i18n = require('../../helpers/localization');
+
+const mainMenuMessageTemplate = i18n.t('messages.mainMenu');
 
 module.exports = mainMenuMessageTemplate;
